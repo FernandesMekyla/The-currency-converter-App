a3f5f4e9dd822aacd8a01e9f8fb56393ff4779c3
Mekyla Fernandes <11179263@college.wlc.ac.uk>
2024-03-26 14:51:16 +0000

    saveeeeee

---
+++ The currency converter App/Exchange rate calculator.vb
@@ -0,0 +1,29 @@
+﻿Imports System.IO
+
+Public Class Form1
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.
+
+        Dim filePath As String = ""
+
+
+        If File.Exists(filePath) Then
+
+            Dim lines As String() = File.ReadAllLines(filePath)
+
+
+            If lines.Length >= 3 Then
+
+                TextBox1.Text = lines(0)
+                TextBox2.Text = lines(1)
+                TextBox3.Text = lines(2)
+
+
+            Else
+                MessageBox.Show("Not enough lines in the file to fill all text boxes.")
+            End If
+        Else
+            MessageBox.Show("File does not exist.")
+        End If
+    End Sub
+End Class
